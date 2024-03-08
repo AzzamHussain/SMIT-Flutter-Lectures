940c322bad141861b04ae48924836fdb947f6134
AzzamHussain <azzamhussain70@gmail.com>
2024-03-09 02:18:31 +0500

    Beginning

--- azzam/lib/loginview.dart
+++ azzam/lib/loginview.dart
@@ -10,13 +10,32 @@ class loginview extends StatelessWidget {
         backgroundColor: Colors.amber,
         title: Text("data"),
       ),
-      body: Container(
-        color: Colors.red,
-        height: 150,
-        width: 200,
-        child: Text('Azzam'),
+      body: Center(
+        child: Container(
+          alignment: Alignment(-1, -1),
+          width: 300,
+          height: 150,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.black,
+              width: 5,
+            ), //border all
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Text(
+            "Hello",
+            style: TextStyle(fontSize: 32),
+          ), //Text
+        ), //container
       ),
     );
+    //   body: Container(
+    //     color: Colors.red,
+    //     height: 150,
+    //     width: 200,
+    //     child: Text('Azzam'),
+    //   ),
+    // );
 
 //     body: Container(
 // color: Colors.red,
