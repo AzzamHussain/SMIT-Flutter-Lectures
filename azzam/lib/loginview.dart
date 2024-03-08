e413633dad8a85d761aff1152db0138a3250f9b1
AzzamHussain <azzamhussain70@gmail.com>
2024-03-09 02:30:54 +0500

    Expanded

--- azzam/lib/loginview.dart
+++ azzam/lib/loginview.dart
@@ -6,28 +6,41 @@ class loginview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: Text("data"),
-      ),
-      body: Center(
-        child: Container(
-          alignment: Alignment(-1, -1),
-          width: 300,
-          height: 150,
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.black,
-              width: 5,
-            ), //border all
-            borderRadius: BorderRadius.circular(20),
+      body: Row(
+        children: [
+          Expanded(
+            child: Container(color: Colors.blueGrey),
           ),
-          child: Text(
-            "Hello",
-            style: TextStyle(fontSize: 32),
-          ), //Text
-        ), //container
+          Expanded(
+            child: Container(
+              color: Colors.deepOrangeAccent,
+            ),
+          )
+        ],
       ),
+
+      // appBar: AppBar(
+      //   backgroundColor: Colors.amber,
+      //   title: Text("data"),
+      // ),
+      // body: Center(
+      //   child: Container(
+      //     alignment: Alignment(-1, -1),
+      //     width: 300,
+      //     height: 150,
+      //     decoration: BoxDecoration(
+      //       border: Border.all(
+      //         color: Colors.black,
+      //         width: 5,
+      //       ), //border all
+      //   ,    borderRadius: BorderRadius.circular(20),
+      //     ),
+      //     child: Text(
+      //       "Hello",
+      //       style: TextStyle(fontSize: 32),
+      //     ), //Text
+      //   ), //container
+      // )
     );
     //   body: Container(
     //     color: Colors.red,
