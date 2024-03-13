8793d7a1be732be4c3683206125907949306821f
AzzamHussain <azzamhussain70@gmail.com>
2024-03-13 17:15:22 +0500

    assets

--- azzam/lib/main.dart
+++ azzam/lib/main.dart
@@ -12,17 +12,27 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Azzam hussain"),
-        ),
-        body: const loginview(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Define the action for the floating action button here
-          },
-          child: const Icon(Icons.add),
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text("Azzam hussain"),
+          ),
+          body: Image.network(
+              "https://www.pexels.com/photo/blue-bmw-sedan-near-green-lawn-grass-170811/")
+
+          // body: Container(
+          //   color: Colors.deepPurple,
+          //   margin: EdgeInsets.all //https://www.pexels.cto/blue-bmw-sedan-near-green-lawn-grass-170811/(10),
+          //   padding: EdgeInsets.all(10),
+          //   height: 100,
+          //   width: 100,
+          // ),
+          // body: const loginview(),
+          // floatingActionButton: FloatingActionButton(
+          //   onPressed: () {
+          //     // Define the action for the floating action button here
+          //   },
+          //   child: const Icon(Icons.add),
+          // ),
+          ),
     );
   }
 }
