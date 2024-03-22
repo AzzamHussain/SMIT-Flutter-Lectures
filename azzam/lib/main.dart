bf8b19978ca30f4f228f2df2887ea9e2684efea7
AzzamHussain <azzamhussain70@gmail.com>
2024-03-22 23:38:14 +0500

    ListTile

--- azzam/lib/main.dart
+++ azzam/lib/main.dart
@@ -1,48 +1,48 @@
 import 'package:azzam/loginview.dart';
 import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
-// class counterView extends StatefulWidget{
-//   const counterView({super.key});
+void main() {
+  runApp(const MyApp());
+}
+class counterView extends StatefulWidget{
+  const counterView({super.key});
 
-//   @override
-//   State<counterView> createState() => _counterViewState();
-// }
+  @override
+  State<counterView> createState() => _counterViewState();
+}
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//           appBar: AppBar(
-    //         title: const Text("Azzam hussain"),
-    //       ),
-    //       body: Image.network(
-    //           "https://www.pexels.com/photo/blue-bmw-sedan-near-green-lawn-grass-170811/")
-    //           //const Textfield(
-    //           // style,bold
-    //           //)
-    //           //elevated button , text button
-    //           //button properties: onpressed,child
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Azzam hussain"),
+          ),
+          body: Image.network(
+              "https://www.pexels.com/photo/blue-bmw-sedan-near-green-lawn-grass-170811/")
+              //const Textfield(
+              // style,bold
+              //)
+              //elevated button , text button
+              //button properties: onpressed,child
 
-    //       // body: Container(
-    //       //   color: Colors.deepPurple,
-    //       //   margin: EdgeInsets.all(10),
-    //       //   padding: EdgeInsets.all(10),
-    //       //   height: 100,
-    //       //   width: 100,
-    //       // ),
-    //       // body: const loginview(),
-    //       // floatingActionButton: FloatingActionButton(
-    //       //   onPressed: () {
-    //       //     // Define the action for the floating action button here
-    //       //   },
-    //       //   child: const Icon(Icons.add),
-    //       // ),
-    //       ),
-    // );
-  
+          // body: Container(
+          //   color: Colors.deepPurple,
+          //   margin: EdgeInsets.all(10),
+          //   padding: EdgeInsets.all(10),
+          //   height: 100,
+          //   width: 100,
+          // ),
+          // body: const loginview(),
+          // floatingActionButton: FloatingActionButton(
+          //   onPressed: () {
+          //     // Define the action for the floating action button here
+          //   },
+          //   child: const Icon(Icons.add),
+          // ),
+          ),
+    );
+}}
---
+++ newclass/android/app/src/main/kotlin/com/example/newclass/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.newclass
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
