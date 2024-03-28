5ccc3b2d92d9977dbaf56cec5fa4ec1635869945
AzzamHussain <azzamhussain70@gmail.com>
2024-03-28 18:17:32 +0500

    Ramazan Last Class

--- newclass/lib/counterview.dart
+++ newclass/lib/counterview.dart
@@ -8,23 +8,37 @@ class _todoState extends StatefulWidget {
 }
 
 class __todoStateState extends State<_todoState> {
+
+  List studentsNames=["azzam","ramis","hasan","arshad"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
+      // body: ListView.builder(
+      //   // children: [
+      //   //   Container(
+      //   //     color: Colors.red,
+      //   //     height: 200,
+      //   //     width: 200,
+      //   //   ),
+      //   //   Container(
+      //   //     color: Colors.black26,
+      //   //     height: 200,
+      //   //     width: 200,
+      //   //   )
+      //   //],
+      // ),
+      
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: ( BuildContext context,int  index) {
+          return Container(
+            margin: const EdgeInsets.only(bottom: 10),
             color: Colors.red,
             height: 200,
-            width: 200,
-          ),
-          Container(
-            color: Colors.black26,
-            height: 200,
-            width: 200,
-          )
-        ],
-      ),
-    );
+            child: Text("$studentsNames[index]"),
+          );
+          
+        },
+    ),);
   }
 }
